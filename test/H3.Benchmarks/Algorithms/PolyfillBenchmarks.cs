ecea0fe5c15c5c4447cb6e2ac0beb75bc7a181cf
Ken N. March <knmarch@implant.net>
2021-10-22 23:50:46 -0600

    why not 6.0 as well...?

--- test/H3.Benchmarks/Algorithms/PolyfillBenchmarks.cs
+++ test/H3.Benchmarks/Algorithms/PolyfillBenchmarks.cs
@@ -11,6 +11,7 @@ using GeoCoord = H3.Model.GeoCoord;
 
 namespace H3.Benchmarks.Algorithms {
 
+    [SimpleJob(RuntimeMoniker.Net60)]
     [SimpleJob(RuntimeMoniker.Net50)]
     [SimpleJob(RuntimeMoniker.NetCoreApp31)]
     [SimpleJob(RuntimeMoniker.Net48)]
